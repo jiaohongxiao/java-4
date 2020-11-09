f0d177a9abbf04e44f4481a860fe81cac0a2b844
jiaohongxiao <72740540+jiaohongxiao@users.noreply.github.com>
2020-11-09 21:05:53 +0800

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,18 @@
+
+public class Interface {      //interface 接口
+	public interface TeacherInterface {
+		// 设置教师的工资
+		public abstract void setPay(float pay);
+		// 获取教师的工资
+		public abstract void getPay(float pay);
+	}
+	public interface StudentInterface {
+		// 设置学生的学费
+		public abstract void setFee(float fee);
+		// 获取学生的学费
+		public abstract void getFee(float fee);
+	}
+	
+}
+
+
